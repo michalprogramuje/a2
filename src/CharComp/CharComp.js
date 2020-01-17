c36ec3279f8772ba53e49e814eb31489157e5788
mzlotorzynski <zloty667@gmail.com>
2020-01-17 15:24:51 +0100

    added charComp, not working yet though

---
+++ src/CharComp/CharComp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CharComp.css';
+
+const charComp = (props) =>
+  return (
+    <div className = 'CharComp'>
+      <p>{props.char}</p>
+    </div>
+  )
+
+export default charComp;
